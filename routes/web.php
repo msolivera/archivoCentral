6dd110c071a889ddd05d8347c03dcacc8829f9d6
msolivera <32486643+msolivera@users.noreply.github.com>
2022-04-03 21:29:01 -0300

    Reparacion de bug, paginas repetidas

--- routes/web.php
+++ routes/web.php
@@ -13,20 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Auth::routes();
-
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('admin', function(){
-    return view('admin.dashboard');
-});
 Route::get('/login', function () {
     return view('login');
 });
