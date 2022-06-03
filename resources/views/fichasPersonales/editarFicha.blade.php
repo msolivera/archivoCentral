c7ddc867445be75559b96f03fd612a5c371598ee
msolivera <32486643+msolivera@users.noreply.github.com>
2022-06-03 09:55:30 -0300

    crear profesiones en ficha

--- resources/views/fichasPersonales/editarFicha.blade.php
+++ resources/views/fichasPersonales/editarFicha.blade.php
@@ -963,21 +963,46 @@
 <!-- Modal -->
 <div class="modal fade" id="profesionModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">profesionModal</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
+    <form method="POST" action="{{ route('fichasPersonalesProfesion.store', $fichaPer) }}">
+        {{ csrf_field() }}
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+                            aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title" id="profesionLabel"></h4>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+  
+                        <div class="col-md-6" style="display: inline-block;">
+  
+                            <div class="form-group">
+                                <label for="profesion">Profesion</label>
+                                <select name="profesion" class="form-control select2" style="width: 100%;"
+                                    id="profesion">
+                                    <option value=""> Seleccione </option>
+                                    @foreach ($profesiones as $profesion)
+                                        <option value="{{ $profesion->id }}">
+                                            {{ $profesion->nombre }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label for="observacion">Observación</label>
+                                <input name="observacion" type="imput" class="form-control" id="observacion"
+                                    placeholder="..." value="{{ old('observacion') }}">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+                        <button class="btn btn-primary">Guardar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
   </div>
 </div>
 
