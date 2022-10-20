552eaefd38280a1ec58a507ee8435b960d7f581a
msolivera <32486643+msolivera@users.noreply.github.com>
2022-10-20 12:53:25 -0300

    c

--- database/migrations/2022_05_18_151804_create_dossiers_table.php
+++ database/migrations/2022_05_18_151804_create_dossiers_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('dossiers', function (Blueprint $table) {
             $table->id();
+            //$table->longText('description');
             $table->timestamps();
         });
     }
